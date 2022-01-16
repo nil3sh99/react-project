10d53e5b797c1ccb9b6329d33c93b2f033172563
Nilesh Aggarwal <agnilesh8199@gmail.com>
2022-01-16 00:47:03 -0500

    Created local DB + Working routers and new components added

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,25 +1,44 @@
-const BlogList = ({blogProp, title, handleDeleteProp}) => {
-    //props is the object and we are using this object to link to a certain key from props parent
-    // const blogs = props.blogProp;
-    // const title = props.title;
-    // const handleDelete = props.deleteProp;
-    // const handleArchive = props.archiveProp;
+// const BlogList = ({blogProp, title, handleDeleteProp}) => {
+//     //props is the object and we are using this object to link to a certain key from props parent
+//     // const blogs = props.blogProp;
+//     // const title = props.title;
+//     // const handleDelete = props.deleteProp;
+//     // const handleArchive = props.archiveProp;
 
 
-    return (  
-        <div className="blog-list">
-            <h2>{title}</h2>
-            {blogProp.map(blog => (
-               <div className="blog-preview" key={blog.id}>
+//     return (  
+//         <div className="blog-list">
+//             <h2>{title}</h2>
+//             {blogProp.map(blog => (
+//                <div className="blog-preview" key={blog.id}>
                    
-                   <h2>{blog.title}</h2>
-                   <p>Written by {blog.author}</p>
-                   <button onClick={() => handleDeleteProp(blog.id)}>Delete blog</button>
+//                    <h2>{blog.title}</h2>
+//                    <p>Written by {blog.author}</p>
+//                    <button onClick={() => handleDeleteProp(blog.id)}>Delete blog</button>
                    
-               </div>
-           ))}
+//                </div>
+//            ))}
+//         </div>
+//     );
+// }
+ 
+// export default BlogList;
+
+import { Link } from 'react-router-dom';
+
+const BlogList = ({ blogs }) => {
+  return (
+    <div className="blog-list">
+      {blogs.map(blog => (
+        <div className="blog-preview" key={blog.id} >
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{ blog.title }</h2>
+            <p>Written by { blog.author }</p>
+          </Link>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
  
 export default BlogList;
--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,35 +1,16 @@
-import { useState } from 'react';
-import BlogList from './BlogList';
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
-    const [blogs, setBlogs] = useState([
-        {title: 'a', body: 'abc', author: 'A', id: 1},
-        {title: 'b', body: 'abc', author: 'B', id: 2},
-        {title: 'c', body: 'abc', author: 'A', id: 3}
-    ]);
-
-    const [lastDeletedBlog, setLastDeletedBlog] = useState([
-        {title: '', body: ' ', author: '', id: null}
-    ])
-    console.log(blogs);
-    const handleDelete = (id) =>{
-        const newBlogs = blogs.filter(x => x.id !== id);
-        const lastDeletedBlog = blogs.filter(x => x.id === id);
-        setLastDeletedBlog(lastDeletedBlog);
-        setBlogs(newBlogs);
-        
-    }
-
-    return ( 
-        <div className = "home">           
-           <BlogList blogProp = {blogs} title = "All Blogs" handleDeleteProp={handleDelete}></BlogList>
-           {/* Useful when we want to do search filter */}
-           {/* <BlogList blogProp = {blogs.filter((blog) => blog.author === 'A')} title = "A's Blogs"></BlogList> */}
-           <BlogList blogProp = {lastDeletedBlog} title = "Last Deleted Blog"></BlogList>
-
-        </div>
-    );
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
+  );
 }
-
+ 
 export default Home;
